81d4add7ea99a09221eb9514ba2845cf15d744ef
Carlos C Soto <eclipxe13@gmail.com>
2019-03-31 16:39:25 -0600

    Implementación del servicio `stamped`

---
+++ src/Services/Stamping/StampedService.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Services\Stamping;
+
+use PhpCfdi\Finkok\Definitions\Services;
+use PhpCfdi\Finkok\FinkokSettings;
+
+class StampedService
+{
+    /** @var FinkokSettings */
+    private $settings;
+
+    public function __construct(FinkokSettings $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function settings(): FinkokSettings
+    {
+        return $this->settings;
+    }
+
+    public function stamped(StampingCommand $command): StampingResult
+    {
+        $soapCaller = $this->settings()->createCallerForService(Services::stamping());
+        $rawResponse = $soapCaller->call('stamped', [
+            'xml' => $command->xml(),
+        ]);
+        $result = new StampingResult('stampedResult', $rawResponse);
+        return $result;
+    }
+}
---
+++ tests/Unit/Services/Stamping/StampedServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Tests\Unit\Services\Stamping;
+
+use PhpCfdi\Finkok\Services\Stamping\StampingCommand;
+use PhpCfdi\Finkok\Services\Stamping\StampedService;
+use PhpCfdi\Finkok\Tests\Fakes\FakeSoapFactory;
+use PhpCfdi\Finkok\Tests\TestCase;
+
+class InvokeStampedServiceTest extends TestCase
+{
+    public function testStampedSendXmlAndProcessPreparedResult(): void
+    {
+        $preparedResult = json_decode(TestCase::fileContentPath('stamped-response-with-alerts.json'));
+
+        $soapFactory = new FakeSoapFactory();
+        $soapFactory->preparedResult = $preparedResult;
+
+        $settings = $this->createSettingsFromEnvironment($soapFactory);
+        $service = new StampedService($settings);
+        $command = new StampingCommand('foo');
+
+        $result = $service->stamped($command);
+        $this->assertSame('FAKE1', $result->alerts()->first()->errorCode());
+
+        $caller = $soapFactory->latestSoapCaller;
+        $this->assertSame('stamped', $caller->latestCallMethodName);
+        $this->assertArrayHasKey('xml', $caller->latestCallParameters);
+        $this->assertSame('foo', $caller->latestCallParameters['xml']);
+    }
+}
