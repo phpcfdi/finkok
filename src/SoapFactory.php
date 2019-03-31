b5b93bcd289fcb4ec6cfab6885935f34c1b2c30e
Carlos C Soto <eclipxe13@gmail.com>
2019-03-31 01:19:28 -0600

    Agregar opciones al SoapClient al crearlo

--- src/SoapFactory.php
+++ src/SoapFactory.php
@@ -11,6 +11,9 @@ class SoapFactory
     public function create(string $endpoint): SoapClient
     {
         return new SoapClient($endpoint . '?singleWsdl', [
+            'features' => SOAP_SINGLE_ELEMENT_ARRAYS,
+            'cache_wsdl' => WSDL_CACHE_MEMORY,
+            'exceptions' => true,
             'trace' => true,
         ]);
     }
