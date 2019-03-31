12b85123fb1241c202fedbd63ebe60b07133ec6e
Carlos C Soto <eclipxe13@gmail.com>
2019-03-31 12:10:00 -0600

    Agregar soporte para archivos .env en testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,9 @@ error_reporting(-1);
 
 // require composer autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
+
+call_user_func(function (): void {
+    /** @noinspection PhpFullyQualifiedNameUsageInspection */
+    $dotenv = new \Symfony\Component\Dotenv\Dotenv();
+    $dotenv->load(__DIR__ . '/.env');
+});
