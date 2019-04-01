7dcef3dfe926be5c25c1c6bc420bde1449a8061f
Carlos C Soto <eclipxe13@gmail.com>
2019-04-01 16:09:15 -0600

    Soporte para psr/log en SoapCaller y SoapFactory
    En el ticket de finkok me han pedido la *evidencia* del ticket #17287

--- src/SoapCaller.php
+++ src/SoapCaller.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace PhpCfdi\Finkok;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use RuntimeException;
 use SoapClient;
 use stdClass;
 use Throwable;
 
-class SoapCaller
+class SoapCaller implements LoggerAwareInterface
 {
     /** @var SoapClient */
     private $soapClient;
@@ -17,10 +20,14 @@ class SoapCaller
     /** @var array */
     private $extraParameters;
 
+    /** @var LoggerInterface */
+    private $logger;
+
     public function __construct(SoapClient $soapClient, array $extraParameters = [])
     {
         $this->soapClient = $soapClient;
         $this->extraParameters = $extraParameters;
+        $this->logger = new NullLogger();
     }
 
     private function soapClient(): SoapClient
@@ -38,14 +45,37 @@ class SoapCaller
         $finalParameters = $this->finalParameters($parameters);
         $soap = $this->soapClient();
         try {
-            return $soap->__soapCall($methodName, [$finalParameters]);
+            $result = $soap->__soapCall($methodName, [$finalParameters]);
+            $this->logger->debug(strval(json_encode([
+                $methodName => $this->extractSoapClientTrace($soap),
+            ], JSON_PRETTY_PRINT)));
+            return $result;
         } catch (Throwable $exception) {
+            $this->logger->error(strval(json_encode(
+                ['method' => $methodName, 'parameters' => $finalParameters] + $this->extractSoapClientTrace($soap),
+                JSON_PRETTY_PRINT
+            )));
             throw new RuntimeException(sprintf('Fail soap call to %s', $methodName), 0, $exception);
         }
+    }
+
+    protected function extractSoapClientTrace(SoapClient $soapClient): array
+    {
+        return [
+            'request.headers' => @$soapClient->__getLastRequestHeaders(),
+            'request.body' => @$soapClient->__getLastRequest(),
+            'response.headers' => @$soapClient->__getLastResponseHeaders(),
+            'response.body' => @$soapClient->__getLastResponse(),
+        ];
     }
 
     public function finalParameters(array $parameters): array
     {
         return array_merge($parameters, $this->extraParameters());
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 }
