c43153ad5852670a43d62ebede2dc6c1391b8752
Carlos C Soto <eclipxe13@gmail.com>
2019-05-07 12:37:31 -0500

    Agregar FIEL del RFC provisto por Finkok
    Agregar la conversión a archivos PEM de la fiel

--- tests/create-pem-files.php
+++ tests/create-pem-files.php
@@ -7,24 +7,36 @@ use PhpCfdi\Finkok\Tests\TestCase;
 
 require __DIR__ . '/bootstrap.php';
 
-$cerFile = TestCase::filePath('certs/TCM970625MB1.cer');
-$cerFilePem = TestCase::filePath('certs/TCM970625MB1.cer.pem');
-$keyFile = TestCase::filePath('certs/TCM970625MB1.key');
-$passPhrase = trim(TestCase::fileContentPath('certs/TCM970625MB1.password.bin'));
-$keyFilePem = TestCase::filePath('certs/TCM970625MB1.key.pem');
-
 $openSsl = new OpenSSL();
 
-if (file_exists($cerFilePem)) {
-    echo "$cerFilePem already exists\n";
-} else {
-    $openSsl->derCerConvert($cerFile, $cerFilePem);
-    echo "$cerFilePem created\n";
-}
+$converter = function (string $cerFile, string $keyFile, string $passPhrase) use ($openSsl): void {
+    $cerFilePem = $cerFile . '.pem';
+    $keyFilePem = $keyFile . '.pem';
+    if (file_exists($cerFilePem)) {
+        echo "$cerFilePem already exists\n";
+    } else {
+        $openSsl->derCerConvert($cerFile, $cerFilePem);
+        echo "$cerFilePem created\n";
+    }
 
-if (file_exists($keyFilePem)) {
-    echo "$keyFilePem already exists\n";
-} else {
-    $openSsl->derKeyProtect($keyFile, $passPhrase, $keyFilePem, $passPhrase);
-    echo "$keyFilePem created\n";
-}
+    if (file_exists($keyFilePem)) {
+        echo "$keyFilePem already exists\n";
+    } else {
+        $openSsl->derKeyProtect($keyFile, $passPhrase, $keyFilePem, $passPhrase);
+        echo "$keyFilePem created\n";
+    }
+};
+
+// CSD
+$converter(
+    TestCase::filePath('certs/TCM970625MB1.cer'),
+    TestCase::filePath('certs/TCM970625MB1.key'),
+    trim(TestCase::fileContentPath('certs/TCM970625MB1.password.bin'))
+);
+
+// FIEL
+$converter(
+    TestCase::filePath('fiel/TCM970625MB1.cer'),
+    TestCase::filePath('fiel/TCM970625MB1.key'),
+    trim(TestCase::fileContentPath('fiel/TCM970625MB1.password.bin'))
+);
