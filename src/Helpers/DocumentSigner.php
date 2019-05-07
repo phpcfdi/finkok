3ed80d8f561fe3949e06f145d8238bf13cd431af
Carlos C Soto <eclipxe13@gmail.com>
2019-05-07 10:52:04 -0500

    Crear un objeto de ayuda para firmar los documentos con formato Finkok

---
+++ src/Helpers/DocumentSigner.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Helpers;
+
+use DateTimeImmutable;
+use DOMDocument;
+use RobRichards\XMLSecLibs\XMLSecurityDSig;
+use RobRichards\XMLSecLibs\XMLSecurityKey;
+
+class DocumentSigner
+{
+    /** @var string */
+    private $rfc;
+
+    /** @var DateTimeImmutable */
+    private $date;
+
+    /** @var string */
+    private $content;
+
+    public function __construct(string $rfc, DateTimeImmutable $date, string $content)
+    {
+        $this->rfc = $rfc;
+        $this->date = $date;
+        $this->content = $content;
+    }
+
+    public function rfc(): string
+    {
+        return $this->rfc;
+    }
+
+    public function date(): DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function content(): string
+    {
+        return $this->content;
+    }
+
+    public function sign(string $certificateFile, string $privateKeyFile, string $passPhrase): string
+    {
+        $document = new DOMDocument('1.0', 'UTF-8');
+        $root = $document->createElement('documento');
+        $document->appendChild($root);
+        $contract = $document->createElement('contrato', $this->content());
+        $contract->setAttribute('rfc', $this->rfc());
+        $contract->setAttribute('fecha', $this->date()->format('Y-m-d\TH:i:s'));
+        $root->appendChild($contract);
+        $document->normalizeDocument();
+
+        $objDSig = new XMLSecurityDSig();
+        $objDSig->setCanonicalMethod(XMLSecurityDSig::EXC_C14N);
+        $objDSig->addReference(
+            $document,
+            XMLSecurityDSig::SHA1,
+            ['http://www.w3.org/2000/09/xmldsig#enveloped-signature'],
+            ['force_uri' => true]
+        );
+
+        $objKey = new XMLSecurityKey(XMLSecurityKey::RSA_SHA1, ['type' => 'private']);
+        $objKey->passphrase = $passPhrase; // set passphrase before loading key
+        $objKey->loadKey($privateKeyFile, true, false);
+
+        $objDSig->sign($objKey);
+        $objDSig->add509Cert($certificateFile, true, true);
+
+        $objDSig->appendSignature($document->documentElement);
+        return $document->saveXML();
+    }
+}
---
+++ tests/Unit/Helpers/DocumentSignerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Tests\Unit\Helpers;
+
+use DateTimeImmutable;
+use PhpCfdi\Finkok\Helpers\DocumentSigner;
+
+use PhpCfdi\Finkok\Tests\TestCase;
+
+class DocumentSignerTest extends TestCase
+{
+    public function testCreateDocumentSigner(): void
+    {
+        $rfc = 'COSC8001137NA';
+        $date = new DateTimeImmutable('2019-01-13 14:15:16');
+        $content = 'Lorem Ipsum';
+
+        $certificate = $this->filePath('certs/TCM970625MB1.cer');
+        $privateKey = $this->filePath('certs/TCM970625MB1.key.pem');
+        $passPhrase = trim($this->fileContentPath('certs/TCM970625MB1.password.bin'));
+
+        $docSigner = new DocumentSigner($rfc, $date, $content);
+        $this->assertSame($rfc, $docSigner->rfc());
+        $this->assertSame($date, $docSigner->date());
+        $this->assertSame($content, $docSigner->content());
+        $signed = $docSigner->sign($certificate, $privateKey, $passPhrase);
+
+        // the comparison does not check white spacing,
+        // lorem-ipsum-signed.xml is formatted for better reading
+        $this->assertXmlStringEqualsXmlFile($this->filePath('lorem-ipsum-signed.xml'), $signed);
+    }
+}
