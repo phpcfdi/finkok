6a913c87baa9939ee7ffe199cb7d40b6925e2a28
Carlos C Soto <eclipxe13@gmail.com>
2019-05-07 20:39:35 -0500

    Implementación de obtención de clientes (faltan pruebas de integración)

---
+++ src/Services/Registration/ObtainService.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Services\Registration;
+
+use PhpCfdi\Finkok\Definitions\Services;
+use PhpCfdi\Finkok\FinkokSettings;
+
+class ObtainService
+{
+    /** @var FinkokSettings */
+    private $settings;
+
+    public function __construct(FinkokSettings $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    public function settings(): FinkokSettings
+    {
+        return $this->settings;
+    }
+
+    public function obtain(ObtainCommand $command): ObtainResult
+    {
+        $this->settings()->changeUsernameKey('reseller_username');
+        $this->settings()->changeUsernameKey('reseller_password');
+        $soapCaller = $this->settings()->createCallerForService(Services::stamping());
+        $rawResponse = $soapCaller->call('get', array_filter([
+            'taxpayer_id' => $command->rfc(),
+        ]));
+        return new ObtainResult($rawResponse);
+    }
+}
