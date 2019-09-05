4ef142a4af63356869c3533dc19a1ed9c7a12b36
Carlos C Soto <eclipxe13@gmail.com>
2019-09-04 19:29:03 -0500

    Fix Finkok helper: accesor name is not the same as service method name

--- tests/Unit/FinkokTest.php
+++ tests/Unit/FinkokTest.php
@@ -8,6 +8,9 @@ use BadMethodCallException;
 use InvalidArgumentException;
 use PhpCfdi\Finkok\Finkok;
 use PhpCfdi\Finkok\FinkokSettings;
+use PhpCfdi\Finkok\Services\Manifest\GetContractsCommand;
+use PhpCfdi\Finkok\Services\Manifest\GetContractsResult;
+use PhpCfdi\Finkok\Services\Manifest\GetContractsService;
 use PhpCfdi\Finkok\Services\Stamping\StampingCommand;
 use PhpCfdi\Finkok\Services\Stamping\StampingResult;
 use PhpCfdi\Finkok\Services\Stamping\StampService;
@@ -100,5 +103,75 @@ class FinkokTest extends TestCase
             . ' but received ' . get_class($command)
         );
         $finkok->{'getContracts'}($command);
+    }
+
+    public function testExecuteServiceWithCallNameDifferentFromServiceMethodName(): void
+    {
+        /** @var FinkokSettings&MockObject $settings */
+        $settings = $this->createMock(FinkokSettings::class);
+        /** @var Finkok&MockObject $finkok */
+        $finkok = new class($settings) extends Finkok {
+            public function executeService(string $method, $service, $command)
+            {
+                $result = parent::executeService($method, $service, $command);
+                return $result;
+            }
+        };
+
+        /** @var MockObject $command */
+        $command = $this->createMock(GetContractsCommand::class);
+        /** @var MockObject $service */
+        $service = $this->createMock(GetContractsService::class);
+
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessageRegExp('/The service \w+ does not have a method foo$/');
+        $finkok->{'executeService'}('foo', $service, $command);
+    }
+
+    public function testInvokingOneMappedMagicMethodWithDifferentName(): void
+    {
+        /** @var GetContractsResult $result */
+        $result = $this->createMock(GetContractsResult::class);
+        /** @var GetContractsCommand $command */
+        $command = $this->createMock(GetContractsCommand::class);
+        /** @var GetContractsService $command */
+        $service = $this->createMock(GetContractsService::class);
+        $service->expects($this->once())->method('obtainContracts')->willReturn($result);
+
+        /** @var FinkokSettings&MockObject $settings */
+        $settings = $this->createMock(FinkokSettings::class);
+        /** @var Finkok&MockObject $finkok */
+        $finkok = $this->getMockBuilder(Finkok::class)
+            ->enableOriginalConstructor()
+            ->setConstructorArgs([$settings])
+            ->onlyMethods(['createService'])
+            ->getMock();
+        $finkok->method('createService')->willReturn($service);
+
+        // this test looks stupid, but means that it was able to override the method name to call on service
+        $this->assertSame($result, $finkok->getContracts($command));
+    }
+
+    public function testServicesMapHaveCorrectNamedAttribute(): void
+    {
+        /** @var FinkokSettings&MockObject $settings */
+        $settings = $this->createMock(FinkokSettings::class);
+        $exposer = new class($settings) extends Finkok {
+            public function exposeServicesMap(): array
+            {
+                return parent::SERVICES_MAP;
+            }
+
+            public function exposeCreateService(string $method)
+            {
+                return $this->createService($method);
+            }
+        };
+        $servicesMap = $exposer->exposeServicesMap();
+        foreach ($servicesMap as $methodName => $definition) {
+            $finalMethodName = $definition[2] ?? $methodName;
+            $service = $exposer->exposeCreateService($methodName);
+            $this->assertTrue(is_callable([$service, $finalMethodName]));
+        }
     }
 }
