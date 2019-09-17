bb3cded5130431d9aed7000475de8fce7a9f7e8c
Carlos C Soto <eclipxe13@gmail.com>
2019-09-17 18:42:43 -0500

    Make RfcPacEnviaSolicitud attribute configurable on constructor

--- src/Helpers/GetRelatedSigner.php
+++ src/Helpers/GetRelatedSigner.php
@@ -11,6 +11,8 @@ use PhpCfdi\XmlCancelacion\DOMSigner;
 
 class GetRelatedSigner
 {
+    public const DEFAULT_PACRFC = 'CVD110412TF6';
+
     /** @var string */
     private $uuid;
 
@@ -20,11 +22,23 @@ class GetRelatedSigner
     /** @var RfcRole */
     private $role;
 
-    public function __construct(string $uuid, string $rfc, RfcRole $role)
+    /** @var string */
+    private $pacRfc;
+
+    /**
+     * GetRelatedSigner constructor.
+     *
+     * @param string $uuid
+     * @param string $rfc
+     * @param RfcRole|null $role If null (ommited) then uses emitter role
+     * @param string $pacRfc If empty (ommited) then uses DEFAULT_PACRFC
+     */
+    public function __construct(string $uuid, string $rfc, RfcRole $role = null, string $pacRfc = self::DEFAULT_PACRFC)
     {
         $this->uuid = $uuid;
         $this->rfc = $rfc;
         $this->role = $role ?? RfcRole::emitter();
+        $this->pacRfc = $pacRfc ?: static::DEFAULT_PACRFC;
     }
 
     public function uuid(): string
@@ -42,6 +56,11 @@ class GetRelatedSigner
         return $this->role;
     }
 
+    public function pacRfc(): string
+    {
+        return $this->pacRfc;
+    }
+
     public function createDocumentToSign(): DOMDocument
     {
         $document = new DOMDocument('1.0', 'UTF-8');
@@ -50,7 +69,7 @@ class GetRelatedSigner
         $root->setAttributeNS($xmlns, 'xmlns:xsd', 'http://www.w3.org/2001/XMLSchema');
         $root->setAttributeNS($xmlns, 'xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
         $root->setAttribute('RfcEmisor', ($this->role->isEmitter()) ? $this->rfc : '');
-        $root->setAttribute('RfcPacEnviaSolicitud', 'CVD110412TF6');
+        $root->setAttribute('RfcPacEnviaSolicitud', $this->pacRfc);
         $root->setAttribute('RfcReceptor', ($this->role->isRecipient()) ? $this->rfc : '');
         $root->setAttribute('Uuid', $this->uuid);
         $document->appendChild($root);
@@ -61,6 +80,11 @@ class GetRelatedSigner
     public function sign(string $certificateFile, string $privateKeyFile, string $passPhrase): string
     {
         $credentials = new Credentials($certificateFile, $privateKeyFile, $passPhrase);
+        return $this->signUsingCredentials($credentials);
+    }
+
+    public function signUsingCredentials(Credentials $credentials): string
+    {
         $document = $this->createDocumentToSign();
         $domSigner = new DOMSigner($document);
         $domSigner->sign($credentials);
