78ed6e8bf2b58489c5688a298bc3bdee25475da6
Carlos C Soto <eclipxe13@gmail.com>
2019-09-28 05:04:29 -0500

    Try again to cancel if CodStatus is "UUID Not Found"

--- tests/Integration/Services/Cancel/CancelServicesTest.php
+++ tests/Integration/Services/Cancel/CancelServicesTest.php
@@ -47,9 +47,11 @@ class CancelServicesTest extends IntegrationTestCase
                 $this->fail('StatusCode 304: "Certificado revocado o caduco", do you must change the CSD?');
             }
             // do not try again if a SAT issue is **not** found
-            // 708: Fink ok cannot connect to SAT
+            // 708: Finkok cannot connect to SAT
             // 205: SAT does not have the uuid available for cancellation
-            if ('708' !== $result->statusCode() && '205' !== $document->documentStatus()) {
+            // UUID Not Found: WTF SAT, why do you don't have the UUID in your system?
+            if (! in_array($result->statusCode(), ['708', 'UUID Not Found'], true)
+                && '205' !== $document->documentStatus()) {
                 break;
             }
             // do not try again if in the loop for more than allowed
