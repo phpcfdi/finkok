900bf15321c9a33e1de7106adef77910f79d5268
Carlos C Soto <eclipxe13@gmail.com>
2019-10-25 00:38:54 -0500

    Add helper GetSatStatusExtractor

---
+++ src/Helpers/GetSatStatusExtractor.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Helpers;
+
+use DOMDocument;
+use PhpCfdi\CfdiExpresiones\DiscoverExtractor;
+use PhpCfdi\Finkok\Services\Cancel\GetSatStatusCommand;
+
+class GetSatStatusExtractor
+{
+    /** @var string[] */
+    private $expressionData;
+
+    /**
+     * GetSatStatusExtractor constructor.
+     *
+     * @param array $expressionData
+     */
+    public function __construct(array $expressionData)
+    {
+        $this->expressionData = [
+            're' => strval($expressionData['re'] ?? ''),
+            'rr' => strval($expressionData['rr'] ?? ''),
+            'tt' => strval($expressionData['tt'] ?? ''),
+            'id' => strval($expressionData['id'] ?? ''),
+        ];
+    }
+
+    public static function fromXmlDocument(DOMDocument $document)
+    {
+        $discoverer = new DiscoverExtractor();
+        return new self($discoverer->obtain($document));
+    }
+
+    public static function fromXmlString(string $xmlCfdi): self
+    {
+        $document = new DOMDocument();
+        $document->loadXML($xmlCfdi);
+        return static::fromXmlDocument($document);
+    }
+
+    public function buildCommand(): GetSatStatusCommand
+    {
+        $expData = $this->expressionData;
+        return new GetSatStatusCommand($expData['re'], $expData['rr'], $expData['id'], $expData['tt']);
+    }
+}
---
+++ tests/Unit/Helpers/GetSatStatusExtractorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Tests\Unit\Helpers;
+
+use PhpCfdi\Finkok\Helpers\GetSatStatusExtractor;
+use PhpCfdi\Finkok\Tests\TestCase;
+
+class GetSatStatusExtractorTest extends TestCase
+{
+    public function testConstructWithEmptyData(): void
+    {
+        $extractor = new GetSatStatusExtractor([]);
+        $command = $extractor->buildCommand();
+        $this->assertEmpty($command->rfcIssuer());
+        $this->assertEmpty($command->rfcRecipient());
+        $this->assertEmpty($command->uuid());
+        $this->assertEmpty($command->total());
+    }
+
+    public function testConstructWithFakeCfdi33(): void
+    {
+        $fakeCfdi = <<<EOT
+<cfdi:Comprobante xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                  xmlns:cfdi="http://www.sat.gob.mx/cfd/3"
+                  xmlns:tfd="http://www.sat.gob.mx/TimbreFiscalDigital"
+                  Version="3.3" Total="123.45" Sello="">
+    <cfdi:Emisor Rfc="EKU9003173C9"/>
+    <cfdi:Receptor Rfc="COSC8001137NA"/>
+    <cfdi:Complemento>
+        <tfd:TimbreFiscalDigital UUID="12345678-1234-1234-1234-000000000001"/>
+    </cfdi:Complemento>
+</cfdi:Comprobante>
+EOT;
+        $extractor = GetSatStatusExtractor::fromXmlString($fakeCfdi);
+        $command = $extractor->buildCommand();
+        $this->assertSame('EKU9003173C9', $command->rfcIssuer());
+        $this->assertSame('COSC8001137NA', $command->rfcRecipient());
+        $this->assertSame('12345678-1234-1234-1234-000000000001', $command->uuid());
+        $this->assertSame('123.45', $command->total());
+    }
+}
