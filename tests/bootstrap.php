69f5230c65c6007af5478593281b795fff054be8
Carlos C Soto <eclipxe13@gmail.com>
2019-12-05 12:11:08 -0600

    Improve tests bootstrap loading .env file
    symfony dotenv will not use putenv by default from version 5

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,20 +3,28 @@
 declare(strict_types=1);
 
 // report all errors
-use Symfony\Component\Dotenv\Dotenv;
-
 error_reporting(-1);
 
+// set time zone to mexico city instead of running on default UTC
 date_default_timezone_set('America/Mexico_City');
 
 // require composer autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
 
-call_user_func(function (): void {
-    /** @noinspection PhpFullyQualifiedNameUsageInspection */
-    $envFile = __DIR__ . '/.env';
-    if (file_exists($envFile) && ! is_dir($envFile)) {
-        $dotenv = new Dotenv();
-        $dotenv->load($envFile);
+call_user_func(new class() {
+    public function __invoke(): void
+    {
+        $environmentFile = __DIR__ . '/.env';
+        if (! $this->environmentFileExists($environmentFile)) {
+            trigger_error(sprintf('Cannot read testing environment file %s', $environmentFile), E_USER_NOTICE);
+            return;
+        }
+        $dotDevUsePutenv = true;
+        (new Symfony\Component\Dotenv\Dotenv($dotDevUsePutenv))->load($environmentFile);
+    }
+
+    public function environmentFileExists(string $environmentFile): bool
+    {
+        return file_exists($environmentFile) && ! is_dir($environmentFile) && is_readable($environmentFile);
     }
 });
