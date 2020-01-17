e0437854554c67845577ba0a01437214c76e5b03
Carlos C Soto <eclipxe13@gmail.com>
2020-01-17 10:44:20 -0600

    Make retentions StampCommand extended from Stamping\StampingCommand

--- src/Services/Retentions/StampCommand.php
+++ src/Services/Retentions/StampCommand.php
@@ -4,18 +4,8 @@ declare(strict_types=1);
 
 namespace PhpCfdi\Finkok\Services\Retentions;
 
-class StampCommand
+use PhpCfdi\Finkok\Services\Stamping\StampingCommand;
+
+class StampCommand extends StampingCommand
 {
-    /** @var string */
-    private $xml;
-
-    public function __construct(string $xml)
-    {
-        $this->xml = $xml;
-    }
-
-    public function xml(): string
-    {
-        return $this->xml;
-    }
 }
