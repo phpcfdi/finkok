90134fef66c70981a768188a2222906c7d07b414
Carlos C Soto <eclipxe13@gmail.com>
2020-01-24 15:12:39 -0600

    Add missing unit test QueryPending command & result

---
+++ tests/Unit/Services/Stamping/QueryPendingCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Tests\Unit\Services\Stamping;
+
+use PhpCfdi\Finkok\Services\Stamping\QueryPendingCommand;
+use PhpCfdi\Finkok\Tests\TestCase;
+
+class QueryPendingCommandTest extends TestCase
+{
+    public function testCreateCommand(): void
+    {
+        $uuid = '11111111-2222-3333-4444-000000000001';
+        $command = new QueryPendingCommand($uuid);
+        $this->assertSame($uuid, $command->uuid());
+    }
+}
---
+++ tests/Unit/Services/Stamping/QueryPendingResultTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\Finkok\Tests\Unit\Services\Stamping;
+
+use PhpCfdi\Finkok\Services\Stamping\QueryPendingResult;
+use PhpCfdi\Finkok\Tests\TestCase;
+
+class QueryPendingResultTest extends TestCase
+{
+    public function testUsingKnownStampResponse(): void
+    {
+        $data = json_decode($this->fileContentPath('querypending-response.json'));
+        $response = new QueryPendingResult($data);
+        $this->assertSame('S', $response->status());
+        $this->assertSame('<xml/>', $response->xml());
+        $this->assertSame('uuid', $response->uuid());
+        $this->assertSame('uuid_status', $response->uuidStatus());
+        $this->assertSame('next_attempt', $response->nextAttempt());
+        $this->assertSame('1', $response->attempts());
+        $this->assertSame('error', $response->error());
+        $this->assertSame('date', $response->date());
+    }
+}
